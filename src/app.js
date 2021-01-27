fc67ed2d8f3a13afc9c60410aa493683b704e806
Sergey <serge.dmitriev@gmail.com>
2021-01-27 17:16:55 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import Vue from 'vue/dist/vue.js';
 import Settings from './components/settings.vue';
+import Card from './components/card.vue';
 import store from './stores/index';
 
 Vue.config.devtools = true;
 Vue.config.silent = true;
 
 const Widget = {
-    render() {
+    render(self) {
+        console.log('render');
+        self.render_template({
+            render: '<div id="{{ el }}"> {{el}}</div>',
+          }, {el: Card.$el});
+
+        const vm = new Vue({
+          render: h => h(Card),
+        }).$mount(Card.$el);
+
+        console.log(vm);
         return true;
     },
     init() {
@@ -22,10 +33,6 @@ const Widget = {
             store: store,
             render: h => h(Settings),
         }).$mount(Settings.$el);
-
-        // After you create app
-        window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = window.vue.constructor;
-
         console.log('oneDocs settings');
     },
     onSave() {
---
+++ src/components/card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>hello, {{ title }}</p>
+    </div>
+</template>
+
+<script>
+    import { Component, Vue } from 'vue-property-decorator';
+    // import { mapActions } from 'vuex';
+
+    @Component({
+        name: "Card",
+        computed: {
+        }
+    })
+
+    export default class Card extends Vue {
+        static $el = 'card_element';
+
+        data() {
+          return {
+              title: 'Card'
+          };
+        }
+    }
+</script>
